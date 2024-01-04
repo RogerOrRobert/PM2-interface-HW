de347e3f14460456e3095c1c15136edfde6c3014
rogerProtofy <roger.lopez@protofy.xyz>
2024-01-04 17:42:02 +0100

    ecosystem with python backend updated

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,13 @@ module.exports = {
       ...require('./apps/api/pm2.config.js').apps,
       ...require('./apps/proxy/pm2.config.js').apps,
       ...require('./apps/next/pm2.config.js').apps,
-      ...require('./apps/admin/pm2.config.js').apps
+      ...require('./apps/admin/pm2.config.js').apps,
+      {
+        name: 'app.py',
+        script: './python-scripts/app.py',
+        interpreter: '/usr/bin/python',
+        exec_mode: 'fork',
+        error_file: './pm2_mqttError.log'
+      }
     ]
 };
